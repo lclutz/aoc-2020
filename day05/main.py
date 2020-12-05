17c3e7b57ea9885fb4e2410b3e157299d3280903
Lorenz Lutz <lorenz@mklutz.de>
2020-12-05 19:11:06 +0100

    day05

---
+++ day05/main.py
@@ -0,0 +1,36 @@
+# Solution for Advent of Code 2020 Day 05
+
+class BoardingPass:
+    s = ""
+
+    def __init__(self, s):
+        self.s = s
+
+    def __repr__(self) -> str:
+        return self.s
+
+    def getSeatID(self):
+        tmp = (self.s.replace("L", "0")
+               .replace("F", "0")
+               .replace("B", "1")
+               .replace("R", "1"))
+
+        return int(tmp, 2)
+
+
+def main():
+    input = []
+    with open("input.txt") as file:
+        input = file.read().strip().split("\n")
+
+    boarding_pass_ids = [BoardingPass(x).getSeatID() for x in input]
+
+    print("part 1: %d" % max(boarding_pass_ids))
+
+    for i in range(min(boarding_pass_ids), max(boarding_pass_ids)):
+        if i not in boarding_pass_ids:
+            print("part 2: %d" % i)
+
+
+if __name__ == "__main__":
+    main()
